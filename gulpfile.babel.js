a2423f410cea4cb3628ddc31cf0ed26d48db5e8f
dagori <mariyav@protonmail.com>
2020-03-06 22:04:20 +0300

    init

---
+++ gulpfile.babel.js
@@ -0,0 +1,121 @@
+const { src, dest, series, parallel, watch } = require('gulp');
+const babel = require('gulp-babel');
+const cleanCSS = require('gulp-clean-css');
+const del = require('del');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+const autoprefixer = require('gulp-autoprefixer');
+// const imagemin = require('gulp-imagemin');
+const rename = require('gulp-rename');
+const svgSprite = require('gulp-svg-sprite');
+const plumber = require('gulp-plumber');
+// const uglify = require('gulp-uglify');
+// const htmlmin = require('gulp-htmlmin');
+const sourcemaps = require('gulp-sourcemaps');
+
+const config = {
+  shape: {
+    dimension: { // Set maximum dimensions
+      maxWidth: 26,
+      maxHeight: 26,
+    },
+    spacing: { // Add padding
+      padding: 5,
+    },
+  },
+  mode: {
+    view: { // Activate the «view» mode
+      bust: false,
+      render: {
+        scss: true, // Activate Sass output (with default options)
+      },
+    },
+    symbol: false, // Activate the «symbol» mode
+  },
+};
+
+function sprites() {
+  return src('src/images/icon-*.svg')
+    .pipe(svgSprite(config))
+    .pipe(dest('build/images'));
+}
+
+function styles() {
+  return src('src/scss/style.scss', { sourcemaps: true })
+    .pipe(plumber())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      overrideBrowserList: ['last 2 versions', '>1%'],
+      cascade: false,
+    }))
+    .pipe(cleanCSS({
+      level: 2,
+    }))
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(dest('build/css', { sourcemaps: '.' }))
+    .pipe(browserSync.stream());
+}
+
+function scripts() {
+  return src('src/js/*.js', { sourcemaps: true })
+    .pipe(sourcemaps.init())
+    .pipe(plumber())
+    .pipe(babel())
+    // .pipe(uglify())
+    .pipe(rename({ extname: '.min.js' }))
+    .pipe(dest('build/js', { sourcemaps: '.' }))
+    .pipe(browserSync.stream());
+}
+// [ ">0.25%", "not ie 11", "not op_mini all"]
+// function images() {
+//   return src('src/images/**/*.+(jpg|jpeg|png|svg|webp)')
+//     .pipe(imagemin([
+//       imagemin.svgo({
+//         plugins: [
+//           { removeViewBox: false },
+//           { cleanupIDs: false },
+//         ],
+//       }),
+//       imagemin.mozjpeg({ quality: 75, progressive: true }),
+//       imagemin.optipng({ optimizationLevel: 7 }),
+//     ]))
+//     .pipe(dest('build/images'));
+// }
+
+// function lib() {
+//   return src('vendor/*.js')
+//   .pipe(dest('build/js'))
+// }
+
+function serve() {
+  browserSync.init({
+    server: {
+      baseDir: './',
+    },
+  });
+  watch('*.html').on('change', browserSync.reload);
+  // watch('src/images/*', series(images));
+  watch('src/js/*.js', series(scripts));
+  watch('src/scss/*.scss', series(styles));
+}
+
+function clean() {
+  return del(['build/**']);
+}
+
+function font() {
+  return src('src/fonts/*')
+    .pipe(dest('build/fonts'));
+}
+
+// Экспорт для вызова через консоль
+exports.serve = serve;
+exports.css = styles;
+exports.js = scripts;
+// exports.img = images;
+exports.clean = clean;
+exports.sprites = sprites;
+exports.build = series(clean, parallel(font, styles, scripts));
+exports.default = series(clean, parallel(font, styles, scripts, serve));
+// exports.build = series(clean, images, parallel(font, styles, lib, scripts));
+// exports.default = series(clean, images, parallel(font, styles, lib, scripts, serve));
